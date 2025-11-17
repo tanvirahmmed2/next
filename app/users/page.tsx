b8a3236ce77a321257c3f8376a50d8504993ee59
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-11-17 21:11:57 +0600

    update

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -6,11 +6,12 @@ interface User{
 }
 
 const UsersPage = async() => {
-  const res= await fetch('https://jsonplaceholder.typicode.com/users')
+  const res= await fetch('https://jsonplaceholder.typicode.com/users', {cache: 'no-store'})
   const users: User[]= await res.json()
   return (
     <div>
       This is a users page
+      <p>{new Date().toLocaleTimeString()}</p>
       {users.map((e)=> {return( <p key={e.id}>{e.name}</p>)})}
     </div>
   )
