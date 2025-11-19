51642f3f706b1debad568560420fd26c465efc32
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-11-19 19:55:27 +0600

    update

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <div className='w-full flex flex-row items-center justify-between p-2 bg-black text-white'>
       <Link href='/'>Next</Link>
-      <div>
+      <div className='flex flex-row gap-4 items-center justify-center'>
         <Link href='/dashboard/users'>Users</Link>
         <Link href='/dashboard/analytics'>Analytics</Link>
         <Link href='/about'>About</Link>
