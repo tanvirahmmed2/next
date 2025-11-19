8d16bc7ecc3b04cc0c3e053b5aa2a5d0f2bbce68
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-11-19 19:00:36 +0600

    update

--- app/navbar/page.tsx
+++ app/navbar/page.tsx
@@ -1,8 +1,13 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='w-full flex flex-row items-center justify-between p-2'>
+    <div className='w-full flex flex-row items-center justify-between p-2 bg-black text-white'>
+      <Link href='/'>Next</Link>
+      <div>
+        <Link href='/users'>Users</Link>
+      </div>
       
     </div>
   )
