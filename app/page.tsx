fbf200c3dd50ae5bfddc15c6d510904544714f69
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-11-20 19:02:09 +0600

    update

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,10 @@ function Home() {
       <h1 className="text-6xl font-semibold text-center">Event You Can not Miss</h1>
       <p className="text-lg ">Hackathon, Meetups and Conferences. All in one</p>
       <ExploreBtn/>
-      <div>
+      <div className="mt-6 space-y-7">
+        {[1,2,3,4].map((event)=>(
+          <p key={event}>Event {event}</p>
+        ))}
 
       </div>
     </section>
