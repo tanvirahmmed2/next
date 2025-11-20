c98082a80c2847e8ab949f155d4228b883313089
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-11-20 14:23:34 +0600

    update

---
+++ app/components/ExploreBtn.tsx
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react'
+
+const ExploreBtn = () => {
+  return (
+    <button>Explore More</button>
+  )
+}
+
+export default ExploreBtn
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 
 import React from 'react'
+import ExploreBtn from './components/ExploreBtn'
 
 
 function Home() {
   return (
     <main>
+      <ExploreBtn/>
     </main>
   )
 }
