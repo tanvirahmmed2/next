14b3472d6b2851168f6871090fbac65dd25e78ba
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-11-24 19:39:18 +0600

    cloudinary

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,6 +1,7 @@
 import { Event } from "@/database";
 import { connectToDatabase } from "@/lib/mongodb";
 import { NextRequest, NextResponse } from "next/server";
+import { v2 as cloudinary } from 'cloudinary'
 
 
 export async function POST(req: NextRequest) {
@@ -15,18 +16,28 @@ export async function POST(req: NextRequest) {
       return NextResponse.json({ message: "Invalid JSON data format", e }, { status: 400 })
     }
 
-    const file= formData.get('image') as File
+    const file = formData.get('image') as File
 
-    if(!file){
-      return NextResponse.json({message: "Image file is required"}, {status:401})
+    if (!file) {
+      return NextResponse.json({ message: "Image file is required" }, { status: 401 })
     }
+
+    const arrayBuffer = await file.arrayBuffer()
+    const buffer = Buffer.from(arrayBuffer)
+    const uploadResult = await new Promise((resolve, reject) => {
+      cloudinary.uploader.upload_stream({ resource_type: 'image', folder: 'next' }, (error, result) => {
+        if (error) return reject(error);
+        resolve(result)
+      }).end(buffer)
+    })
+    event.image=( uploadResult as {secure_url: string}).secure_url
 
     const createdEvent = await Event.create(event)
 
     return NextResponse.json({ message: 'Event created successfully', event: createdEvent }, { status: 201 })
   } catch (e) {
 
-    return NextResponse.json({ message: "Event creation failed", error: e instanceof Error? e.message :"Unknown" }, { status: 400 })
+    return NextResponse.json({ message: "Event creation failed", error: e instanceof Error ? e.message : "Unknown" }, { status: 400 })
   }
 
 }
