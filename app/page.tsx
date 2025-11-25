c0504bdcbe4be2b4b7b77fcf667242f788a04273
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-11-25 17:56:48 +0600

    ui

--- app/page.tsx
+++ app/page.tsx
@@ -2,28 +2,28 @@ import EventCard from "@/components/elements/EventCard"
 import ExploreBtn from "@/components/elements/ExploreBtn"
 import { IEvent } from "@/database"
 
-const BASE_URL=process.env.NEXT_PUBLIC_BASE_URl
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URl
 
 
 
 async function Home() {
 
-  const response= await fetch(`${BASE_URL}/api/events`)
-  const {events}= await response.json()
+  const response = await fetch(`${BASE_URL}/api/events`)
+  const { events } = await response.json()
 
   return (
     <section className="w-full flex flex-col items-center justify-center gap-2 py-12">
-      <div className="w-full flex flex-col items-center justify-center gap-2 min-h-[80vh]">
+      <div className="w-full flex flex-col items-center justify-center gap-2">
         <h1 className="text-6xl font-semibold text-center">The Hub for Every Dev</h1>
-      <h1 className="text-6xl font-semibold text-center">Event You Can not Miss</h1>
-      <p className="text-lg ">Hackathon, Meetups and Conferences. All in one</p>
-      <ExploreBtn/>
+        <h1 className="text-6xl font-semibold text-center">Event You Can not Miss</h1>
+        <p className="text-lg ">Hackathon, Meetups and Conferences. All in one</p>
+        <ExploreBtn />
       </div>
-      
+
       <div className="mt-6 space-y-7 flex flex-wrap gap-4 p-4 justify-center">
-        {events && events.length >0  && events.map((event:IEvent)=>(
+        {events && events.length > 0 && events.map((event: IEvent) => (
           <div key={event.title}>
-            <EventCard {...event}/>
+            <EventCard {...event} />
           </div>
         ))}
 
