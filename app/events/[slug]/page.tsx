076f6e41ec6204c230391b60d7f31699b11b004e
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-11-25 18:55:59 +0600

    update

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -23,28 +23,28 @@ async function Page({ params }: { params: Promise<{ slug: string }> }) {
   const event = result.event || result.data;
 
   return (
-    <div className="w-full flex flex-row items-center justify-center gap-4">
-      <div className=" flex-col flex items-center justify-center gap-4 py-10 px-4">
+    <div className="w-full flex flex-row gap-4  py-10 px-4">
+      <div className="w-3/4 flex-col flex  gap-4">
         <h1 className="text-2xl font-semibold">{event.title}</h1>
         <Image src={event.image} alt={event.title} width={800} height={400} />
         <p>{event.description}</p>
-        <p className="text-center">{event.overview}</p>
-        <div className="flex flex-row items-center gap-4">
-          <p className="flex flex-row items-center gap-4"><Image src={pin} alt="icon" width={15} height={15} className="text-black" />Location: {event.date} - {event.location}</p>
+        <p className="">{event.overview}</p>
+        <div className="flex flex-row gap-4">
+          <p className="flex flex-row gap-4"><Image src={pin} alt="icon" width={15} height={15} className="text-black" />Location: {event.date} - {event.location}</p>
           <p>{event.venue}</p>
 
         </div>
-        <div className="flex flex-row items-center justify-center gap-6">
-          <p className="flex flex-row items-center gap-4"><Image src={calender} alt="icon" width={15} height={15} /> Date: {event.date}</p>
-          <p className="flex flex-row items-center gap-4"> <Image src={clock} alt="icon" width={15} height={15} />Time: {event.time}</p>
+        <div className="flex flex-row gap-6">
+          <p className="flex flex-row gap-4"><Image src={calender} alt="icon" width={15} height={15} /> Date: {event.date}</p>
+          <p className="flex flex-row gap-4"> <Image src={clock} alt="icon" width={15} height={15} />Time: {event.time}</p>
         </div>
 
-        <div className="flex flex-col items-center justify-center gap-2">
+        <div className="flex flex-col gap-2">
           <p>Mode: {event.mode}</p>
           <p> Audience: {event.audience}</p>
         </div>
 
-        <div className="flex flex-col items-center justify-center gap-2">
+        <div className="flex flex-col gap-2">
           <p>Tags: </p>
           {event.tags.map((tag: string, index: number) => (
             <p key={index} className="px-3 p-1 ">{tag}</p>
@@ -52,9 +52,9 @@ async function Page({ params }: { params: Promise<{ slug: string }> }) {
         </div>
       </div>
 
-      <div className="">
+      <div className=" flex items-center justify-center flex-col">
+        <h1 className="w-full text-lg font-semibold">Book event</h1>
 
-        <p>Book event</p>
       </div>
     </div>
   );
