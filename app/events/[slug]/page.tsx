4ed6fc2cdab15d96cb3a09f47946163d773b1b47
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-11-25 18:48:13 +0600

    update

--- app/events/[slug]/page.tsx
+++ app/events/[slug]/page.tsx
@@ -15,7 +15,7 @@ async function Page({ params }: { params: Promise<{ slug: string }> }) {
   });
 
   if (!response.ok) {
-    return <div>Event not found or API error</div>;
+    throw new Error('Event not found')
   }
 
   const result = await response.json();
@@ -24,13 +24,30 @@ async function Page({ params }: { params: Promise<{ slug: string }> }) {
 
   return (
     <div className="w-full flex-col flex items-center justify-center gap-4 py-10 px-4">
-      <Image src={event.image} alt={event.title} width={800} height={400}/>
-      <h1>{event.title}</h1>
+      <h1 className="text-2xl font-semibold">{event.title}</h1>
+      <Image src={event.image} alt={event.title} width={800} height={400} />
       <p>{event.description}</p>
-      <p className="flex flex-row items-center gap-4"><Image src={pin} alt="icon" width={15} height={15} className="text-black"/>Location: {event.date} - {event.location}</p>
+      <p className="text-center">{event.overview}</p>
+      <div className="flex flex-row items-center gap-4">
+        <p className="flex flex-row items-center gap-4"><Image src={pin} alt="icon" width={15} height={15} className="text-black" />Location: {event.date} - {event.location}</p>
+        <p>{event.venue}</p>
+
+      </div>
       <div className="flex flex-row items-center justify-center gap-6">
         <p className="flex flex-row items-center gap-4"><Image src={calender} alt="icon" width={15} height={15} /> Date: {event.date}</p>
         <p className="flex flex-row items-center gap-4"> <Image src={clock} alt="icon" width={15} height={15} />Time: {event.time}</p>
+      </div>
+
+      <div className="flex flex-col items-center justify-center gap-2">
+        <p>Mode: {event.mode}</p>
+        <p> Audience: {event.audience}</p>
+      </div>
+
+      <div className="flex flex-col items-center justify-center gap-2">
+        <p>Tags: </p>
+        {event.tags.map((tag: string, index: number) => (
+          <p key={index} className="px-3 p-1 ">{tag}</p>
+        ))}
       </div>
     </div>
   );
