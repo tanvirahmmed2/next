935749aedbf6d6c4c9ced2f611dc18f1ae0f6d34
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-11-26 18:14:04 +0600

    booking

--- components/elements/BookEvent.tsx
+++ components/elements/BookEvent.tsx
@@ -20,7 +20,7 @@ const BookEvent = () => {
             <form onSubmit={Book}  className='flex flex-col gap-4'>
                 <div className='flex flex-col gap-2'>
                     <label htmlFor="email">Email</label>
-                    <input type="email" id='email' onChange={(e)=> setEmail(e.target.value)} value={email} required className='px-2 p-1 border-2 outline-none rounded-xl'/>
+                    <input type="email" id='email' onChange={(e)=> setEmail(e.target.value)} value={email} required className='px-2 p-1 border-2 outline-none rounded-xl' placeholder='Enter you email'/>
                 </div>
                 <button type='submit' className='px-3 p-1 bg-sky-600 rounded-4xl text-white'>Book now</button>
             </form>
