5b99e7f2290c73a14ff367d9a9b50f5d8a8c3374
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-11-29 15:20:41 +0600

    reset

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,10 @@
-import EventCard from "@/components/elements/EventCard"
-import ExploreBtn from "@/components/elements/ExploreBtn"
-import { IEvent } from "@/database"
+import React from 'react'
 
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URl
-
-
-
-async function Home() {
-
-  const response = await fetch(`${BASE_URL}/api/events`)
-  const { events } = await response.json()
-
+const Home = () => {
   return (
-    <section className="w-full flex flex-col items-center justify-center gap-2 py-12">
-      <div className="w-full flex flex-col items-center justify-center gap-2">
-        <h1 className="text-6xl font-semibold text-center">The Hub for Every Dev</h1>
-        <h1 className="text-6xl font-semibold text-center">Event You Can not Miss</h1>
-        <p className="text-lg ">Hackathon, Meetups and Conferences. All in one</p>
-        <ExploreBtn />
-      </div>
-
-      <div className="mt-6 space-y-7 flex flex-wrap gap-4 p-4 justify-center">
-        {events && events.length > 0 && events.map((event: IEvent) => (
-          <div key={event.title}>
-            <EventCard {...event} />
-          </div>
-        ))}
-
-      </div>
-    </section>
+    <div>
+      New app
+    </div>
   )
 }
 
--- database/index.ts
+++
@@ -1,2 +0,0 @@
-export { Event, type IEvent, type EventDocument } from "./event.model";
-export { Booking, type IBooking, type BookingDocument } from "./booking.model";
