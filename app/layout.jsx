6123f85267feafb0452cfa469040583e0cc12055
Tanvir Ahmmed <tanvir004006@gmail.com>
2025-12-10 12:20:50 +0600

    update fix

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export const metadata={
-    title: 'Next',
+    title:'Next',
     description:'Next home page'
 }
 
--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Page = async() => {
+const Page = () => {
   return (
     <div>
       
