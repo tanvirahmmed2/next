4fc15b58ef27e5197ae1925200b694319d791408
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-01-13 02:54:14 +0600

    new file fresh

--- app/api/auth/logout/route.js
+++
@@ -1,5 +0,0 @@
-import { clearSessionCookie } from '../../../../lib/auth';
-
-export async function POST() {
-  return clearSessionCookie();
-}
--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns:[
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com'
-      }
-    ]
-  }
+  /* config options here */
+  reactCompiler: true,
 };
 
 export default nextConfig;
