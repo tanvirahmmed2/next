7487a74cd36c6d8e0d791828e46c2554b986e7b5
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-01-14 22:02:36 +0600

    login api test

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,19 @@
+import LoginForm from '@/component/form/LoginForm'
 import React from 'react'
 
 const Login = () => {
   return (
-    <div>
-      
+    <div className="w-full min-h-screen flex items-center justify-center p-4">
+      <div className="w-auto flex flex-col md:flex-row items-center justify-center border rounded-lg overflow-hidden p-2">
+        <div className="w-full flex flex-col items-center justify-center">
+          <p className=''>Welcome to</p>
+          <h1 className='text-4xl font-semibold'>Next</h1>
+          <p>messaging app</p>
+
+        </div>
+        <LoginForm/>
+      </div>
+
     </div>
   )
 }
---
+++ src/component/form/LoginForm.tsx
@@ -0,0 +1,43 @@
+'use client'
+import axios from 'axios'
+import React, { useState } from 'react'
+
+const LoginForm = () => {
+    const [formData, setFormData]=useState({
+        email:'',
+        password:''
+    })
+
+    const changeHandler=(e: any)=>{
+        const {name, value}= e.target
+        setFormData((prev)=>({...prev,[name]:value}))
+    }
+
+    const loginHandle=async (e:any) => {
+        e.preventDefault()
+        try {
+            const response= await axios.post('/api/user/login', formData, {withCredentials:true})
+            console.log(response)
+            window.location.replace('/profile')
+        } catch (error:any) {
+            console.log(error)
+            alert(error?.response?.data?.message)
+        }
+        
+    }
+    return (
+        <form onSubmit={loginHandle} className=' w-full flex flex-col items-center justify-center gap-2'>
+            <div className='w-full flex flex-col gap-1'>
+                <label htmlFor="email">Email</label>
+                <input type="email" required name='email' id='email' value={formData.email} onChange={changeHandler} className='w-full px-3 p-1 border outline-none rounded-lg' />
+            </div>
+            <div className='w-full flex flex-col gap-1'>
+                <label htmlFor="password">Email</label>
+                <input type="password" required name='password' id='password' value={formData.password} onChange={changeHandler} className='w-full px-3 p-1 border outline-none rounded-lg' />
+            </div>
+            <button type='submit' className='bg-sky-400 px-3 text-white rounded-lg cursor-pointer hover:bg-sky-500'>Login</button>
+        </form>
+    )
+}
+
+export default LoginForm
