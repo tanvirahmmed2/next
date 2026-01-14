b7fb7699fca9301bb0b6fc67bde166c08e7ccfdf
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-01-14 20:41:50 +0600

    message

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,15 +1,48 @@
 import { connectDB } from "@/component/lib/database/db";
+import { isLogin } from "@/component/lib/middleware";
+import Chat from "@/component/lib/model/chat";
 import { NextResponse } from "next/server";
 
 
-export async function POST(req:Request) {
+export async function POST(req: Request) {
     try {
         await connectDB()
+        const auth = await isLogin()
+        if (!auth.success) {
+            return NextResponse.json({
+                success: false, message: auth.message
+            }, { status: 400 })
+        }
+
+        const userId = auth.payload._id
+
+        const { otherUserId } = await req.json()
+
+        if (!otherUserId) {
+            return NextResponse.json({
+                success: false, message: 'Id not recieved'
+            }, { status: 400 })
+        }
+
+        let chat = await Chat.findOne({
+            isGroup: false,
+            members: { $all: [userId, otherUserId] },
+        });
+
+        if (!chat) {
+            chat = await Chat.create({
+                members: [userId, otherUserId],
+                isGroup: false,
+            });
+        }
+
+        return NextResponse.json({ success: true, chat });
+
     } catch (error: any) {
         return NextResponse.json({
-            success:false, message:error.message
-        },{status:500})
-        
+            success: false, message: error.message
+        }, { status: 500 })
+
     }
-    
+
 }
---
+++ src/app/api/message/route.ts
@@ -0,0 +1,42 @@
+import { connectDB } from "@/component/lib/database/db";
+import { isLogin } from "@/component/lib/middleware";
+import Chat from "@/component/lib/model/chat";
+import Message from "@/component/lib/model/message";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    await connectDB();
+
+    const auth = await isLogin();
+    if (!auth.success) {
+      return NextResponse.json({ success: false, message: auth.message }, { status: 401 });
+    }
+
+    const { chatId, text } = await req.json();
+
+    if (!chatId || !text) {
+      return NextResponse.json({ success: false, message: "Chat id and text are required" }, { status: 400 });
+    }
+
+    const chat = await Chat.findById(chatId);
+    if (!chat || !chat.members.includes(auth.payload._id)) {
+      return NextResponse.json({ success: false, message: "Unauthorized chat access" }, { status: 403 });
+    }
+
+    const newMessage = await Message.create({
+      chatId,
+      senderId: auth.payload._id,
+      text
+    });
+
+    return NextResponse.json({
+      success: true,
+      message: "Message sent",
+      data: newMessage
+    }, { status: 200 });
+
+  } catch (error: any) {
+    return NextResponse.json({ success: false, message: error.message }, { status: 500 });
+  }
+}
