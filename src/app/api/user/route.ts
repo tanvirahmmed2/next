356d23eed03240296e8960ceb445f053e4070d9c
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-01-16 02:22:38 +0600

    chat page

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -5,49 +5,76 @@ import bcrypt from 'bcryptjs'
 
 
 
-export async function POST(req:Request) {
+export async function POST(req: Request) {
     try {
         await connectDB()
 
-        const {name, email, password}= await req.json()
+        const { name, email, password } = await req.json()
 
-        if(!name || !email || !password){
+        if (!name || !email || !password) {
             return NextResponse.json({
-                success: false, message:'Please fill all information'
-            }, {status:400})
+                success: false, message: 'Please fill all information'
+            }, { status: 400 })
         }
 
-        const user= await User.findOne({email})
+        const user = await User.findOne({ email })
 
-        if(user){
+        if (user) {
             return NextResponse.json({
-                success: false, message:'User already exists'
-            }, {status:400})
+                success: false, message: 'User already exists'
+            }, { status: 400 })
         }
 
-        if(password.length <6){
+        if (password.length < 6) {
             return NextResponse.json({
-                success:false, message:'Password must contain atleast 6 characters'
-            },{status:400})
+                success: false, message: 'Password must contain atleast 6 characters'
+            }, { status: 400 })
         }
-        
-        const hasehdPass= await bcrypt.hash(password, 10)
 
-        const newUser= new User({
+        const hasehdPass = await bcrypt.hash(password, 10)
+
+        const newUser = new User({
             name, email, password: hasehdPass
         })
 
         await newUser.save()
 
         return NextResponse.json({
-            success: true, message:'Successfully registered'
-        }, {status:200})
+            success: true, message: 'Successfully registered'
+        }, { status: 200 })
 
     } catch (error: any) {
         return NextResponse.json({
             success: false, message: error.message
-        }, {status:500})
-        
+        }, { status: 500 })
+
     }
-    
+
+}
+
+
+export async function GET() {
+    try {
+        await connectDB()
+
+        const users = await User
+            .find({})
+            .sort({ createdAt: -1 })
+            .select("-password");
+        if (!users || users.length < 1) {
+            return NextResponse.json({
+                success: false, message: 'No user found'
+            }, { status: 400 })
+        }
+
+        return NextResponse.json({
+            success: true, message: 'Successfully fetched users data', payload: users
+        }, { status: 200 })
+    } catch (err: any) {
+        return NextResponse.json({
+            success: false, message: err.message
+        }, { status: 500 })
+
+    }
+
 }
--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,11 +1,91 @@
-import React from 'react'
+'use client'
 
-const CHat = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+import axios from "axios";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { IoIosPersonAdd } from "react-icons/io";
+
+
+interface Chat {
+  _id: string;
+  members: { _id: string; name: string }[];
 }
 
-export default CHat
+interface User {
+  _id: string;
+  name: string;
+}
+
+
+const Chat = () => {
+  const [users, setUsers] = useState<User[]>([])
+  const [chats, setChats] = useState<Chat[]>([])
+
+  useEffect(() => {
+    const fetchChat = async () => {
+      try {
+        const response = await axios.get('/api/user', { withCredentials: true })
+        console.log(response)
+        setUsers(response.data.payload)
+      } catch (error: any) {
+        console.log(error)
+        setUsers([])
+
+      }
+    }
+    fetchChat()
+  }, [])
+
+
+  useEffect(() => {
+    const fetchChat = async () => {
+      try {
+        const response = await axios.get('/api/chat', { withCredentials: true })
+        console.log(response)
+        setChats(response.data.payload)
+      } catch (error: any) {
+        console.log(error)
+        setChats([])
+
+      }
+    }
+    fetchChat()
+  }, [])
+
+  return (
+    <div className="max-w-xl mx-auto p-4">
+      
+      {
+        users!== null && <div>
+          {
+            users.map((user)=>(
+              <div key={user._id}>
+                <p><IoIosPersonAdd/></p>
+                <p>{user.name}</p>
+                <button >Add</button>
+              </div>
+            ))
+          }
+        </div>
+      }
+      
+      
+      
+      
+      
+      <h1 className="text-2xl font-bold mb-4">Chats</h1>
+
+      {chats.map(chat => (
+        <Link
+          key={chat._id}
+          href={`/chat/${chat._id}`}
+          className="block p-3 border rounded mb-2 hover:bg-gray-100"
+        >
+          Chat ID: {chat._id.slice(-6)}
+        </Link>
+      ))}
+    </div>
+  );
+}
+
+export default Chat
