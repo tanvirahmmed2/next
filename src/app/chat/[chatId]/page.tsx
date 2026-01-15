e52bc4acfc57d6711c4838a8437bf2376d2d7081
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-01-16 03:30:47 +0600

    chat id

--- src/app/chat/[chatId]/page.tsx
+++ src/app/chat/[chatId]/page.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+'use client'
 
-const ChatIdPage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+import { IMessage } from "@/component/lib/model/message"
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+const ChatIdPage = ({params}:{params:{chatId:string}}) => {
+    const {chatId}=  params
+    const [messages, setMessages]= useState<IMessage[]>([])
+
+    useEffect(()=>{
+        const fetchMessage=async()=>{
+            try {
+                const response= await axios.get(`/api/message?chatId=${chatId}`, {withCredentials:true})
+                setMessages(response.data.message)
+            } catch (error:any) {
+                console.log(error)
+                setMessages([])
+
+                
+            }
+        }
+        fetchMessage()
+    },[])
+
+    return (
+        <div className='w-full min-h-screen flex flex-col items-center justify-between p-2'>
+        </div>
+    )
 }
 
 export default ChatIdPage
